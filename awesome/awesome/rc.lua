4e7c5d1f444e5301d4aa77c423cc82e4cec10030
Mika Fischer <mika.fischer@kit.edu>
2010-12-07 12:17:18 +0100

    awesome: Don't honor size hints

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -293,6 +293,7 @@ awful.rules.rules = {
       properties = { border_width = beautiful.border_width,
                      border_color = beautiful.border_normal,
                      focus = true,
+                     size_hints_honor = false,
                      keys = clientkeys,
                      buttons = clientbuttons } },
     { rule = { class = "MPlayer" },
