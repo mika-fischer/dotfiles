6097d8532bd8c6b5f3228c294e763421da9b5670
Mika Fischer <fischer@videmo.de>
2012-03-19 12:53:00 +0100

    xmonad: Add keybindings from awesome config

--- xmonad/xmonad/xmonad.hs
+++ xmonad/xmonad/xmonad.hs
@@ -65,4 +65,36 @@ myLogHook = return ()
 myStartupHook = do
     spawn "trayer --edge top --align right --widthtype request --heighttype pixel --height 6 --SetDockType true --SetPartialStrut true --transparent true --alpha 0 --tint 0x000000 --distance -1 --expand true"
 
-myKeys = [("C-M1-l", spawn "xscreensaver-command -lock")]
+home       = "/home/mfischer"
+screenlock = "xscreensaver-command -lock"
+consolekit = "dbus-send --system --print-reply --dest=\"org.freedesktop.ConsoleKit\" /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager."
+upower     = "dbus-send --system --print-reply --dest=\"org.freedesktop.UPower\" /org/freedesktop/UPower org.freedesktop.UPower."
+
+myKeys = [("C-M1-l",                    spawn (screenlock))
+         ,("C-S-M1-l",                  spawn (screenlock))
+         ,("C-S-M1-h",                  spawn (consolekit ++ "Stop"))
+         ,("C-S-M1-r",                  spawn (consolekit ++ "Restart"))
+         ,("C-S-M1-s",                  spawn (upower ++ "Suspend" ++ " & " ++ screenlock))
+         ,("C-S-M1-d",                  spawn (upower ++ "Hibernate" ++ " & " ++ screenlock))
+         ,("<XF86Sleep>",               spawn (upower ++ "Suspend" ++ " & " ++ screenlock))
+         ,("<XF86Suspend>",             spawn (upower ++ "Hibernate" ++ " & " ++ screenlock))
+         ,("M-=",                       spawn (home ++ "/bin/volume.sh up"))
+         ,("M--",                       spawn (home ++ "/bin/volume.sh down"))
+         ,("M-0",                       spawn (home ++ "/bin/volume.sh mute"))
+         ,("<XF86AudioRaiseVolume>",    spawn (home ++ "/bin/volume.sh up"))
+         ,("<XF86AudioLowerVolume>",    spawn (home ++ "/bin/volume.sh down"))
+         ,("<XF86AudioMute>",           spawn (home ++ "/bin/volume.sh mute"))
+         ,("M-<F7>",                    spawn (home ++ "/bin/media.sh prev"))
+         ,("M-<F8>",                    spawn (home ++ "/bin/media.sh next"))
+         ,("M-<F9>",                    spawn (home ++ "/bin/media.sh toggle_play"))
+         ,("M-<F10>",                   spawn (home ++ "/bin/media.sh toggle"))
+         ,("M-<F11>",                   spawn (home ++ "/bin/media.sh volume_down"))
+         ,("M-<F12>",                   spawn (home ++ "/bin/media.sh volume_up"))
+         ,("<XF86AudioPrev>",           spawn (home ++ "/bin/media.sh prev"))
+         ,("<XF86AudioNext>",           spawn (home ++ "/bin/media.sh next"))
+         ,("<XF86AudioPlay>",           spawn (home ++ "/bin/media.sh toggle_play"))
+         ,("M-<XF86AudioRaiseVolume>",  spawn (home ++ "/bin/media.sh volume_up"))
+         ,("M-<XF86AudioLowerVolume>",  spawn (home ++ "/bin/media.sh volume_down"))
+         ,("M-<XF86AudioMute>",         spawn (home ++ "/bin/volume.sh mute"))
+         ]
+
