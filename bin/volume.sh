8916a7705ec3f45e5fa3bfad0f043b30fd06489a
Mika Fischer <fischer@videmo.de>
2012-03-19 12:51:47 +0100

    sh: Fix volume.sh script to work with pulseaudio

--- bin/volume.sh
+++ bin/volume.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-if pgrep -x amixer >/dev/null; then
+if pgrep -u $USER -x amixer >/dev/null; then
     exit 0
+fi
+
+if pgrep -u $USER -x pulseaudio >/dev/null; then
+    inc=1000
+else
+    inc=2
 fi
 
 case $1 in
     up)
-        amixer -q set Master 2+
+        amixer -q set Master ${inc}+
         ;;
     down)
-        amixer -q set Master 2-
+        amixer -q set Master ${inc}-
         ;;
     mute)
         amixer -q set Master toggle
