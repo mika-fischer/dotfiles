b0a4cefa4e9ee76a0d6991ef137ecf5480ef1fb0
Mika Fischer <mika.fischer@zoopnet.de>
2013-01-21 23:56:26 +0100

    awesome: Don't float gimp anymore, since it has a sane layout now

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -324,11 +324,6 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
-    { rule = { class = "gimp" },
-      properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
     { rule = { class = "Skype" },
       properties = { floating = true } },
     { rule = { class = "Git-gui" },
