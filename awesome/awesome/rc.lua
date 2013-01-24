922c1bfaee286618955b06b169630f4dad46e757
Mika Fischer <mika.fischer@zoopnet.de>
2013-01-24 22:44:43 +0100

    awesome: Fix border hiding logic

--- awesome/awesome/rc.lua
+++ awesome/awesome/rc.lua
@@ -357,23 +357,29 @@ end)
 client.connect_signal("focus",   function(c) c.border_color = beautiful.border_focus  end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
-for s = 1, screen.count() do screen[s]:connect_signal("arrange", function ()
-    local clients = awful.client.visible(s)
-    local layout  = awful.layout.getname(awful.layout.get(s))
+for s = 1, screen.count() do
+    screen[s]:connect_signal("arrange", function ()
+        local clients = awful.client.visible(s)
+        local tiled   = awful.client.tiled(s)
+        local layout  = awful.layout.getname(awful.layout.get(s))
 
-    if #clients > 0 then -- Fine grained borders and floaters control
-        for _, c in pairs(clients) do
-            -- Floaters always have borders
-            if awful.client.floating.get(c) or layout == "floating" then
-                c.border_width = beautiful.border_width
-            -- No borders with only one visible client
-            elseif #clients == 1 or layout == "max" then
-                clients[1].border_width = 0
-            else
-                c.border_width = beautiful.border_width
+        if #clients > 0 then
+            for _, c in pairs(clients) do
+                -- Maximized windows never have a border
+                if c.maximized_horizontal and c.maximized_vertical then
+                    c.border_width = 0
+                -- Floating windows always have a border
+                elseif awful.client.floating.get(c) or layout == "floating" then
+                    c.border_width = beautiful.border_width
+                -- No borders with only one visible tiled client or max layout
+                elseif #tiled == 1 or layout == "max" then
+                    c.border_width = 0
+                -- Otherwise (more than one tiled client), draw borders
+                else
+                    c.border_width = beautiful.border_width
+                end
             end
         end
-    end
-end)
-     end
+    end)
+end
 -- }}}
