ea1360233939f5035b2f3f74eb5bded2ad7282b8
Mika Fischer <mika.fischer@zoopnet.de>
2016-01-26 11:50:33 +0100

    bin: Fix typo in spotify.sh

--- bin/spotify.sh
+++ bin/spotify.sh
@@ -5,7 +5,7 @@ playpause() {
 }
 
 prev() {
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previuos
+    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous
 }
 
 next() {
