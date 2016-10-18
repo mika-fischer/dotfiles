b8a5396cd686394881c604b4b63806e39d40b62a
Mika Fischer <mika.fischer@zoopnet.de>
2016-10-18 15:01:34 +0200

    systemd: Add systemd user units

--- bin/dotfile-symlinks.py
+++ bin/dotfile-symlinks.py
@@ -15,8 +15,8 @@ except ImportError:
     exit(1)
 
 
-XDG_CONFIG_DIRS = ('awesome', 'fontconfig', 'gtk3', 'qtile', 'taffybar',
-                   'volumeicon', 'zathura')
+XDG_CONFIG_DIRS = ('awesome', 'fontconfig', 'gtk3', 'qtile', 'systemd',
+                   'taffybar', 'volumeicon', 'zathura')
 
 
 class App(object):
